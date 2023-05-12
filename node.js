b5095071990634e0af874042d7738363ac4285d6
r i s e <95631896+RisingGlitch@users.noreply.github.com>
2023-05-12 16:42:09 +0000

    Update node.js

--- node.js
+++ node.js
@@ -42,5 +42,38 @@ app.get('/register-sw.js', function(req, res) {
     res.sendFile(path.join(__dirname, '/uv/register-sw.js'));
 });
 
+
+import { createServer } from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+
+const bare = createBareServer("/bare/");
+const app = express();
+
+app.use(express.static(publicPath));
+app.use("/uv/", express.static(uvPath));
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.listen({
+  port: 8080,
+});
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
