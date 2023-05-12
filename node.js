463a06fb96160317fd6cd0f4410473a4aaad577c
r i s e <95631896+RisingGlitch@users.noreply.github.com>
2023-05-12 16:51:03 +0000

    Update node.js

--- node.js
+++ node.js
@@ -1,11 +1,35 @@
-const express = require('express');
-const path = require('path');
+import { createServer } from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 
+const bare = createBareServer("/bare/");
 const app = express();
-const port = process.env.PORT || 8080;
 
-app.use('/static', express.static('public'))
-app.use('/static', express.static('uv'))
+app.use(express.static(publicPath));
+app.use("/uv/", express.static(uvPath));
+app.use("/static/", "public");
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.listen({
+  port: 8080,
+});
 
 // sendFile will go here
 app.get('/', function(req, res) {
@@ -43,37 +67,6 @@ app.get('/register-sw.js', function(req, res) {
 });
 
 
-import { createServer } from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-
-const bare = createBareServer("/bare/");
-const app = express();
-
-app.use(express.static(publicPath));
-app.use("/uv/", express.static(uvPath));
-
-const server = createServer();
-
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-server.listen({
-  port: 8080,
-});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
