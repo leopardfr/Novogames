b7c9c8351041d03d5898ce5fcce092d9fc9cf45f
r i s e <95631896+RisingGlitch@users.noreply.github.com>
2023-05-17 17:22:46 +0000

    Games Proxy

--- public/index.js
+++ public/index.js
@@ -34,3 +34,16 @@ form.addEventListener("submit", async (event) => {
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
+
+async function proxySite(url) {
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
