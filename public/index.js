cefcdf500134bd97ccc2ca8c1b27bad7604683f9
leopardfr <142705372+leopardfr@users.noreply.github.com>
2023-10-15 21:14:20 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,17 @@
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var password = document.getElementById("password").value;
+  
+  // Perform password validation here
+  // Example:
+  if (password === "password123") {
+    alert("Password is correct!");
+  } else {
+    alert("Password is incorrect!");
+  }
+});
+
+
 "use strict";
 /**
  * @type {HTMLFormElement}
